c0b4fd76ad75d99e781754141d66f2ef85b027ca
Jason Lieb <Jason.lieb@outlook.com>
2024-03-11 22:10:22 -0400

    get all buttons working

--- main.ts
+++ main.ts
@@ -1,6 +1,11 @@
 import {Editor, MarkdownView, Notice, Plugin} from 'obsidian'
-import {Tab, getTabsFromMarkdown, isDirectoryValid, saveJson} from './src/file-system'
-import {BrowserInterfaceSettingTab} from 'src/settings'
+import {
+  getTabFromMarkdown,
+  getTabsFromMarkdown,
+  isDirectoryValid,
+  saveJson,
+} from './src/file-system'
+import {BrowserInterfaceSettingTab} from './src/settings'
 
 interface BrowserInterfaceSettings {
   browserDirectory: string | undefined
@@ -17,67 +22,97 @@ export default class BrowserInterfacePlugin extends Plugin {
     console.log('Loading Browser Interface Plugin...') // Remove for production
     await this.loadSettings()
     this.setUpCommands()
-    // this.loadMetaBindTemplates()
+    this.loadMetaBindTemplates()
     this.addSettingTab(new BrowserInterfaceSettingTab(this.app, this))
   }
 
   async loadMetaBindTemplates() {
     // @ts-ignore
     const metaBind = this.app.plugins.plugins['obsidian-meta-bind-plugin']
-    console.log({metaBind})
     if (!metaBind) return
 
-    const templates = [
+    type Template = {
+      id?: string
+      label: string
+      style: string
+      actions?: {type: string; command: string}[]
+    }
+
+    const templates: Template[] = [
       {
         id: 'browser-interface-extension-open-window',
-        label: 'Open',
+        label: 'Open Window',
         style: 'primary',
         actions: [{type: 'command', command: 'browser-interface-plugin:open-window'}],
       },
       {
         id: 'browser-interface-extension-open-tab',
-        label: 'Open',
+        label: 'Open Tab',
         style: 'primary',
         actions: [{type: 'command', command: 'browser-interface-plugin:open-tab'}],
       },
       {
         id: 'browser-interface-extension-delete-tab',
-        label: 'Delete',
-        style: 'danger',
+        label: 'Delete Tab',
+        style: 'destructive',
         actions: [{type: 'command', command: 'browser-interface-plugin:delete-tab'}],
       },
     ]
 
-    metaBind.api.buttonManager.setButtonTemplates(templates)
+    const currentTemplates: Template[] = Array.from(
+      metaBind.api.buttonManager.buttonTemplates.values()
+    )
+
+    const otherTemplates = currentTemplates.filter(
+      template => !/^browser-interface-extension-/.test(template.id || '')
+    )
+
+    metaBind.api.buttonManager.setButtonTemplates([...otherTemplates, ...templates])
   }
 
   setUpCommands() {
-    // Change these commands to use checkCallback instead of editorCallback?
     this.addCommand({
       id: 'open-window',
       name: 'Open Window',
-      editorCallback: this.open(getTabsFromMarkdown),
+      editorCallback: (editor: Editor, view: MarkdownView) => {
+        const directory = this.getBrowserDirectory()
+        const tabs = getTabsFromMarkdown(editor.getValue())
+        saveJson(JSON.stringify(tabs), directory)
+        if (view.file !== null) this.app.vault.delete(view.file)
+      },
     })
 
     this.addCommand({
       id: 'open-tab',
       name: 'Open Tab',
-      // editorCallback: this.open(getTabFromMarkdown),
+      editorCallback: (editor: Editor) => {
+        const lineNum = editor.getCursor().line
+        const lineText = editor.getLine(lineNum)
+        if (lineText.split(' ')[1]?.[0] !== '!') return
+
+        const directory = this.getBrowserDirectory()
+        const tab = getTabFromMarkdown(lineText)
+        saveJson(JSON.stringify(tab), directory)
+
+        editor.replaceRange('', {line: lineNum, ch: 0}, {line: lineNum + 1, ch: 0})
+      },
+    })
+
+    this.addCommand({
+      id: 'delete-tab',
+      name: 'Delete Tab',
+      editorCallback: (editor: Editor) => {
+        const lineNum = editor.getCursor().line
+        const lineText = editor.getLine(lineNum)
+        if (lineText.split(' ')[1]?.[0] !== '!') return
+
+        editor.replaceRange('', {line: lineNum, ch: 0}, {line: lineNum + 1, ch: 0})
+      },
     })
   }
 
-  open(getTabsFn: (markdown: string) => Tab[]): (editor: Editor, view: MarkdownView) => void {
-    return (editor: Editor, view: MarkdownView) => {
-      const directory = this.getBrowserDirectory()
-
-      const tabs = getTabsFn(editor.getValue())
-      saveJson(JSON.stringify(tabs), directory)
-      // if (view.file !== null) this.app.vault.delete(view.file)
-    }
-  }
-
   onunload() {
-    this.tearDownQueue
+    this.tearDownQueue()
   }
 
   tearDownQueue() {
--- src/file-system.ts
+++ src/file-system.ts
@@ -1,4 +1,4 @@
-import {TFolder} from 'obsidian'
+import {Editor, MarkdownView, TFolder} from 'obsidian'
 
 export type Tab = {
   favIconUrl: string | undefined
@@ -23,30 +23,31 @@ export function getTabsFromMarkdown(markdown: string): Tab[] {
   return tabs
 }
 
-// export function getTabFromMarkdown(markdown: string): Tab[] {
-//   const lines = markdown.split('\n').filter(line => line.split(' ')[1]?.[0] === '!')
-//   const tabs: Tab[] = lines.map(line => {
-//     const [favIconCell, titleCell] = line.slice(1, -2).split(' | ')
-//     const url = titleCell.slice(titleCell.indexOf('(') + 1, titleCell.indexOf(')'))
-//     const title = titleCell.slice(titleCell.indexOf('[') + 1, titleCell.indexOf(']'))
-//     const favIconUrl = favIconCell.slice(favIconCell.indexOf('(') + 1, favIconCell.indexOf(')'))
-//     return {
-//       favIconUrl,
-//       title,
-//       url,
-//     }
-//   })
-
-//   return tabs
-// }
+export function getTabFromMarkdown(line: string): Tab[] {
+  const [favIconCell, titleCell] = line.slice(1, -2).split(' | ')
+  const url = titleCell.slice(titleCell.indexOf('(') + 1, titleCell.indexOf(')'))
+  const title = titleCell.slice(titleCell.indexOf('[') + 1, titleCell.indexOf(']'))
+  const favIconUrl = favIconCell.slice(favIconCell.indexOf('(') + 1, favIconCell.indexOf(')'))
+  return [
+    {
+      favIconUrl,
+      title,
+      url,
+    },
+  ]
+}
 
 export async function saveJson(json: string, directory: string) {
+  const randomNum = Math.floor(1000 + Math.random() * 9000)
   const existingFile = this.app.vault.getFileByPath(
-    `${directory}/browser-interface-open-queue.json`
+    `${directory}/browser-interface-open-queue-${randomNum}.json`
   )
 
   if (!existingFile) {
-    await this.app.vault.create(`${directory}/browser-interface-open-queue.json`, json + '\n')
+    await this.app.vault.create(
+      `${directory}/browser-interface-open-queue-${randomNum}.json`,
+      json + '\n'
+    )
   } else {
     const currentContent = await this.app.vault.read(existingFile)
     await this.app.vault.modify(existingFile, currentContent + json + '\n')
